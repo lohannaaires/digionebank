9b1b01582be920ec97e4241c694cb242488a8b56
Lohanna Aires <jenwindc@gmail.com>
2021-03-08 23:36:44 -0300

    Add Person class

---
+++ src/main/kotlin/br/com/lohanna/digionebank/Pessoa.kt
@@ -0,0 +1,6 @@
+package br.com.lohanna.digionebank
+
+class Pessoa(
+    val nome: String,
+    val cpf: String
+)
--- src/main/kotlin/br/com/lohanna/digionebank/main.kt
+++ src/main/kotlin/br/com/lohanna/digionebank/main.kt
@@ -1,5 +1,9 @@
 package br.com.lohanna.digionebank
 
 fun main() {
+    val lohanna = Pessoa(nome = "Lohanna", cpf = "111.111.111-11")
+
+    println("Nome: ${lohanna.nome}")
+    println("CPF: ${lohanna.cpf}")
 
 }
