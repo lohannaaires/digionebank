650e6b4c068beed8c87ca3f58beb10ff44f876c8
Lohanna Aires <jenwindc@gmail.com>
2021-03-09 21:19:51 -0300

    Change visibility modifiers
    * Add the "telefone" property to study visibility modifiers
    * Change the visibility modifiers of the "cpf" property and the "telefone" property setter to private
    * Add package to store past tests

--- src/main/kotlin/br/com/lohanna/digionebank/main.kt
+++ src/main/kotlin/br/com/lohanna/digionebank/main.kt
@@ -1,9 +1,13 @@
 package br.com.lohanna.digionebank
 
+import br.com.lohanna.digionebank.teste.testaProperties
+
 fun main() {
     val lohanna = Pessoa(nome = "Lohanna", cpf = "111.111.111-11")
 
-    println("Nome: ${lohanna.nome}")
-    println("CPF: ${lohanna.cpf}")
+    println("${lohanna.infoPessoa()} \n")
 
+    lohanna.alteraTelefone("(99)99999-9999")
+
+    println(lohanna.infoPessoa())
 }
